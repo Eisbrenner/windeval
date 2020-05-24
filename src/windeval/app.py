40a4396e660b0fcd4de4f1f7b116c42f5bb3d6fb
EEisbrenner <gitbrenner@pm.me>
2020-05-24 15:02:21 +0200

    Fix app() test data loading path error
    Running app() in regular environment won't throw an error anymore.
    Instead, the app waits for input before pursuing.

--- src/windeval/app.py
+++ src/windeval/app.py
@@ -16,6 +16,7 @@ paths = [path1, path2]
 
 st.write("# Loaded data sets")
 fmap = {0: "First", 1: "Second"}
+wait = [True, True]
 for i in range(2):
     if not Path(paths[i]).exists() or not paths[i]:
         paths[i] = (
@@ -25,48 +26,51 @@ for i in range(2):
                 "tests/test_data/station_{}.cdf".format(i + 1)
             )
         )
-        st.write(
-            "WARNING: {} file does not exist or none given. Using test path:\n\n".format(
-                fmap[i]
-            ),
-            paths[i],
-        )
-        assert Path(paths[i]).exists()
-wnd = load_product(*[{"path": p} for p in paths], experimental=True)
-st.write("Opened wind products:", wnd)
+        if Path(paths[i]).exists():
+            st.write(
+                "WARNING: {} file does not exist or none given. Using test path:\n\n".format(
+                    fmap[i]
+                ),
+                paths[i],
+            )
+    if Path(paths[i]).exists():
+        wait[i] = False
+    else:
+        st.write("WARNING: {} file does not exist or none given.".format(fmap[i]))
 
+if not any(wait):
+    wnd = load_product(*[{"path": p} for p in paths], experimental=True)
+    st.write("Opened wind products:", wnd)
 
-st.sidebar.markdown("# Select slice")
-st.write("# Selected slices")
+    st.sidebar.markdown("# Select slice")
+    st.write("# Selected slices")
 
+    st.sidebar.markdown("# Select diagnostic method")
+    st.write("# Result")
 
-st.sidebar.markdown("# Select diagnostic method")
-st.write("# Result")
+    variable = st.sidebar.selectbox(
+        "Select variable:", list(wnd[list(wnd.keys())[0]].data_vars)
+    )
+    diag = st.sidebar.selectbox(
+        "Select method:",
+        [
+            a[0]
+            for a in inspect.getmembers(Diagnostics)
+            if not (a[0].startswith("__") and a[0].endswith("__"))
+        ],
+    )
+    assert getattr(Diagnostics, diag) is not None
+    global_diag = {diag: {"variables": variable, "method": "welch", "nperseg": 26}}
 
-variable = st.sidebar.selectbox(
-    "Select variable:", list(wnd[list(wnd.keys())[0]].data_vars)
-)
-diag = st.sidebar.selectbox(
-    "Select method:",
-    [
-        a[0]
-        for a in inspect.getmembers(Diagnostics)
-        if not (a[0].startswith("__") and a[0].endswith("__"))
-    ],
-)
-assert getattr(Diagnostics, diag) is not None
-global_diag = {diag: {"variables": variable, "method": "welch", "nperseg": 26}}
-
-
-calculate = st.button("Calculate")
-if calculate:
-    diagnostics(wnd, **global_diag)
-    for k in wnd.keys():
-        plt.semilogx(wnd[k][variable + "_power_spectral_density"])
-    plt.legend(wnd.keys())
-    plt.title("PSD: power spectral density")
-    plt.xlabel("Frequency")
-    plt.ylabel("Power")
-    plt.tight_layout()
-    plt.grid()
-    st.pyplot()
+    calculate = st.button("Calculate")
+    if calculate:
+        diagnostics(wnd, **global_diag)
+        for k in wnd.keys():
+            plt.semilogx(wnd[k][variable + "_power_spectral_density"])
+        plt.legend(wnd.keys())
+        plt.title("PSD: power spectral density")
+        plt.xlabel("Frequency")
+        plt.ylabel("Power")
+        plt.tight_layout()
+        plt.grid()
+        st.pyplot()
